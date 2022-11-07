f4007bc56e72006e4c5da82430fe9c1c6e115e93
nidhi-dahiya <75343266+nidhi-dahiya@users.noreply.github.com>
2022-11-07 16:22:30 -0500

    more updates

--- app/src/main/java/edu/uga/cs/capitalsquiz/CapitalQuizDBHelper.java
+++ app/src/main/java/edu/uga/cs/capitalsquiz/CapitalQuizDBHelper.java
@@ -8,10 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.util.Log;
 
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 

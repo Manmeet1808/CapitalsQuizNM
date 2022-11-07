3931b90975220c0c562fa0c7c44bbbdbdba9a9c2
Manmeet1808 <55161847+Manmeet1808@users.noreply.github.com>
2022-11-07 18:02:51 -0500

    Recycler and Adapter created and being worked on

---
+++ app/src/main/java/edu/uga/cs/capitalsquiz/MainQuizAdapter.java
@@ -0,0 +1,74 @@
+package edu.uga.cs.capitalsquiz;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class MainQuizAdapter extends AppCompatActivity {
+    public static final String NUMBER_ANSWERED = "num answered";
+    public static final String SCORE = "score";
+    public static final String QUIZ_LIST = "quiz question list";
+
+    //SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+    ActionBar mActionBar;
+
+    static Button submitButton;
+    static Button viewPastResults;
+    static Button newQuiz;
+
+    static RadioGroup radioGroup;
+    static RadioButton rbSelected;
+
+    static List<Questions> fullQuestionsList;
+    static ArrayList<Questions> quizList;
+    static Data quizQuestionsData;
+    static String correctAnswer;
+    public Boolean correct = false;
+    public QuizVariables currentQuiz = new QuizVariables();
+    public Integer numAnswered;
+    public static final String DEBUG_TAG = "DEBUG_QuizQuestions";
+
+    public static ArrayList<String> options;
+    public static ArrayList<String> currentOptions = new ArrayList<>();
+
+    /**
+     * onSaveInstanceState is used to save the variables when the app is stopped abruptly
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        //Need to save num questions answered, score, and quizlist
+        super.onSaveInstanceState(outState);
+        outState.putInt(NUMBER_ANSWERED, currentQuiz.getNumberAnswered());
+        outState.putInt(SCORE, currentQuiz.getScore());
+        outState.putParcelableArrayList(QUIZ_LIST, (ArrayList<? extends Parcelable>) quizList);
+
+    }
+
+}
---
+++ app/src/main/java/edu/uga/cs/capitalsquiz/Recycler.java
@@ -0,0 +1,58 @@
+package edu.uga.cs.capitalsquiz;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Recycler extends RecyclerView.Adapter<Recycler.QuizHolder> {
+    public static final String DEBUG_TAG = "QuizRecyclerAdapter";
+
+    private List<QuizVariables> quizList;
+
+    public Recycler( List<QuizVariables> quizList ) {
+        this.quizList = quizList;
+    }
+
+    class QuizHolder extends RecyclerView.ViewHolder {
+
+        TextView date;
+        TextView score;
+
+        public QuizHolder(View itemView ) {
+            super(itemView);
+
+            date = (TextView) itemView.findViewById( R.id.date );
+            score = (TextView) itemView.findViewById( R.id.score );
+        }
+    }
+
+
+    @Override
+    public QuizHolder onCreateViewHolder(ViewGroup parent, int viewType ) {
+        View view = LayoutInflater.from( parent.getContext()).inflate( R.layout.quiz_result, parent, false );
+        return new QuizHolder( view );
+    }
+
+    @Override
+    public void onBindViewHolder( QuizHolder holder, int position ) {
+        QuizVariables quiz = quizList.get( position );
+
+        Log.d( DEBUG_TAG, "onBindViewHolder: " + quiz );
+
+        holder.score.setText( "" + quiz.getScore() );
+        holder.date.setText( quiz.getDate() );
+    }
+
+    @Override
+    public int getItemCount() {
+        return quizList.size();
+    }
+
+}
